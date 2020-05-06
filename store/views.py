5a70f09e0b39cbfa0f47606aee576b478eeb5ec8
hex-plex <sendhilsom7@gmail.com>
2020-05-07 02:33:49 +0530

    Task2 Done

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from store.models import *
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
