a1b3992c58f1f05432b282476853d3792311079b
hex-plex <sendhilsom7@gmail.com>
2020-05-07 22:20:30 +0530

    register done

--- authentication/views.py
+++ authentication/views.py
@@ -24,4 +24,28 @@ def logoutView(request):
     pass
 
 def registerView(request):
-    pass
+    template_name = 'authentication/signup.html'
+    if request.user.is_authenticated:
+        return redirect('index')
+    if request.method=="GET":
+        return render(request,template_name)
+    else:
+        get_data=request.POST
+        items = [get_data['username']]
+        usererror=User.objects.filter(username__icontains=get_data['username']).first()
+        if usererror:
+            messages.info(request,'This username is unavailable.')
+            return render(request,template_name)
+        emailerror=User.objects.filter(email__icontains=get_data['email']).first()
+        if emailerror:
+            messages.info(request,'This email is already used.')
+            return render(request,template_name)
+        if get_data["password"]!=get_data["password2"]:
+            messages.info(request,'Passwords didnt Match.')
+            return render(request,template_name)
+        newuser = User(username=get_data['username'],email=get_data['email'])
+        newuser.set_password(get_data['password'])
+        newuser.save()
+        user = authenticate(username=get_data['username'],password=get_data['password'])
+        login(request,user)
+        return redirect('index')
